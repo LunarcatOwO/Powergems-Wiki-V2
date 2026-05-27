42bacc97319c5a6b79f9f61215ce90b0bec99715
Lunar <54731257+LunarcatOwO@users.noreply.github.com>
2026-05-27 14:41:15 -0400

    I configured this wrong =v=

--- lib/shared.ts
+++ lib/shared.ts
@@ -5,7 +5,7 @@ export const docsContentRoute = '/llms.mdx/docs';
 
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
-  user: 'iseal',
-  repo: 'powergems',
+  user: 'lunarcatowo',
+  repo: 'powergems-wiki-v2',
   branch: 'main',
 };
